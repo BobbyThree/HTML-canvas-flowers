c3dce6e916d0d37868ec41a7cb0c4a207141527d
bobbythree <mynameisbobbylewis@hotmail.com>
2024-03-16 21:21:24 -0500

    text animates after flower

--- main.js
+++ main.js
@@ -4,16 +4,23 @@ canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
 ctx.globalCompositeOperation = 'destination-over';
 
+//flower variables
 let hue = 0;
 let number = 0;
 let scale = 10;
 
-
+//text variables
 let textY = -10;
-let dy = 5;
+let dy = 3;
+
+function animationHandler() {
+  animateFlower();
+  setTimeout(animateText, 2000);
+}
+animationHandler();
 
 function drawFlower() {
-  let angle = number * 5;
+  let angle = number * 5 ;
   let radius = scale * Math.sqrt(number);
   let posX = radius * Math.sin(angle) + canvas.width / 2;
   let posY = radius * Math.cos(angle) + canvas.height / 2; 
@@ -31,27 +38,25 @@ function drawFlower() {
   hue += 2;
 }
 
-function animateFlower() {
-  //ctx.clearRect(0, 0, canvas.width, canvas.height);
+function animateFlower() {  
   drawFlower();
   if (number > 150) return;
   requestAnimationFrame(animateFlower);
 }
-animateFlower();
+
 
 function drawText() {
   ctx.font = '33px sans-serif';
   ctx.fillStyle = 'lightgreen'
   ctx.textAlign = 'center';
-  ctx.fillText('whattuuuuup', canvas.width / 2, textY);
-    
+  ctx.fillText('Check this shit out!', canvas.width / 2, textY);    
 }
 
 function animateText() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, 100);
   drawText();
   textY += dy;
   if (textY >= 100) return;
   requestAnimationFrame(animateText);
 }
-animateText();
+
